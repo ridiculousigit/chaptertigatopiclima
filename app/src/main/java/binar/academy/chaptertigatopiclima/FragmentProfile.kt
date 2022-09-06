8a6b8805fb4b292368609fac57d6eff635c78883
ridiculousigit <jsigit243@gmail.com>
2022-09-07 00:26:27 +0700

    Initial commit

--- app/src/main/java/binar/academy/chaptertigatopiclima/FragmentProfile.kt
+++ app/src/main/java/binar/academy/chaptertigatopiclima/FragmentProfile.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class FragmentProfile : Fragment() {
 
@@ -14,5 +16,12 @@ class FragmentProfile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnKeluar.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_fragmentProfile_to_fragmentHome)
+        }
     }
 }
